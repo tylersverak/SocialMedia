c8d41298bc57a932322c58a7be536dc15d9af2d1
Tyler S <tyguy4964@gmail.com>
2024-08-23 11:24:37 -0700

    adding gitignore

---
+++ main.py
@@ -0,0 +1,9 @@
+
+from email.mime.text import MIMEText 
+from email.mime.image import MIMEImage 
+from email.mime.application import MIMEApplication 
+from email.mime.multipart import MIMEMultipart 
+import smtplib 
+import os
+
+print("Mishon Compreet")
